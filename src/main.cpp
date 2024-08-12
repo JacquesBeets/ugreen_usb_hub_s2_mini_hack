2fedbdf469f12fbb0339ba66c233bf4df9665208
Jacques Beets <jacques.beets@gmail.com>
2024-08-12 21:49:14 +0200

    working OTA and readme

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
 #include <WebServer.h>
+#include <ElegantOTA.h>
 #include "env.h" // Create this file in your project directory and add your WiFi credentials - this is not very secure as it will be included in your build automatically so be careful
 
 // Replace with your network credentials
@@ -64,7 +66,7 @@ void switchHub() {
 void handleRoot() {
   String html = "<html><head>";
   html += "<meta name='viewport' content='width=device-width, initial-scale=1'>";
-  html += "<style>body{font-family: Arial; text-align: center; color: #ffffff; background:#000000;} #switchButton{background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; text-decoration: none; font-size: 16px; margin: 4px 2px; cursor: pointer;}</style>";
+  html += "<style>body{font-family: Arial; text-align: center; color: #ffffff; background:#000000; padding: 5rem;} #switchButton{background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; text-decoration: none; font-size: 16px; margin: 4px 2px; cursor: pointer;}</style>";
   html += "<script>function updateState() {fetch('/state').then(response => response.text()).then(state => {document.getElementById('state').innerText = state;document.getElementById('switchButton').innerText = 'Switch to ' + (state === 'PC' ? 'Mac' : 'PC');});} setInterval(updateState, 2000);</script>";
   html += "</head><body>";
   html += "<h1>USB Hub Switch</h1>";
@@ -111,6 +113,43 @@ void blinkLED() {
     delay(1000);
 }
 
+// =============================================================================
+// OTA
+// =============================================================================
+void onOTAStart() {
+  // Log when OTA has started
+  Serial.println("OTA update started!");
+}
+
+unsigned long ota_progress_millis = 0;
+void onOTAProgress(size_t current, size_t final) {
+  // Log every 1 second
+  if (millis() - ota_progress_millis > 1000) {
+    ota_progress_millis = millis();
+    Serial.printf("OTA Progress Current: %u bytes, Final: %u bytes\n", current, final);
+    blinkLED();
+  }
+}
+
+void onOTAEnd(bool success) {
+  // Log when OTA has finished
+  if (success) {
+    Serial.println("OTA update finished successfully!");
+  } else {
+    Serial.println("There was an error during OTA update!");
+  }
+}
+
+void elegantOTACallbackInit(void) {
+  ElegantOTA.onStart(onOTAStart);
+  ElegantOTA.onProgress(onOTAProgress);
+  ElegantOTA.onEnd(onOTAEnd);
+}
+
+// =============================================================================
+// Setup
+// =============================================================================
+
 const int MAX_WIFI_CONNECT_ATTEMPTS = 20;
 const int WIFI_RETRY_DELAY = 500; // milliseconds
 
@@ -144,7 +183,7 @@ void setup() {
       while(1) { delay(1000); } // Infinite loop to prevent further execution
     }
   }
-  
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("Hostname: ");
@@ -155,19 +194,30 @@ void setup() {
   // Initialize hubState based on current channel
   updateHubState();
 
-  server.on("/", handleRoot);
-  server.on("/switch", handleSwitch);
-  server.on("/state", handleState);
+  // Route for root / web page
+  server.on("/", HTTP_GET, handleRoot);
+
+  // Route to switch the hub
+  server.on("/switch", HTTP_GET, handleSwitch);
+  
+  // Route to get the current state
+  server.on("/state", HTTP_GET, handleState);
+
+  // Start ElegantOTA
+  ElegantOTA.begin(&server);    // Start ElegantOTA
+  elegantOTACallbackInit();
   server.begin();
 }
 
 void loop() {
+  server.handleClient();
+  ElegantOTA.loop();  
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("WiFi connection lost. Reconnecting...");
     WiFi.reconnect();
   }
+
   updateHubState();
   printWifiStatus();
   // blinkLED();
-  server.handleClient();
 }
